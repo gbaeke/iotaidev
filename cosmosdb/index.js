6c48274a9620073af6baebde7afb23317b869e6e
Geert Baeke <geert@baeke.info>
2018-10-19 17:05:58 +0200

    cosmosdb intro

--- cosmosdb/index.js
+++ cosmosdb/index.js
@@ -1,30 +1,63 @@
 // load environment variables from .env file
 require('dotenv').config();
 
+// load sample tweets
+const tweets = require('./tweets');
+
 const CosmosClient = require('@azure/cosmos').CosmosClient;
 
+// disable SSL verification when you use the emulator
 const client = new CosmosClient({ endpoint: process.env.COSMOSDB_ENDPOINT, 
-    auth: { masterKey: process.env.COSMOSDB_KEY } });
+    auth: { masterKey: process.env.COSMOSDB_KEY },
+    connectionPolicy: { DisableSSLVerification: true }
+});
 
 const databaseId = process.env.COSMOSDB_DATABASE;
-const containerId = process.env.COSMOSDB_COLLECTION
+const containerId = process.env.COSMOSDB_COLLECTION;
+
+// createDatabase - not used
+async function createDatabase() {
+    const { database } = await client.databases.createIfNotExists({ id: databaseId });
+    console.log(`Created database: ${database.id}\n`);
+}
+
+// createContainer - not used
+async function createContainer() {
+    const { container } = await client.database(databaseId).containers.createIfNotExists({ id: containerId });
+    console.log(`Created container: ${container.id}\n`);
+}
+
+// createTweets in container
+async function createTweets() {
+    // do not use a traditional forEach here
+    for ( var tweet of tweets ) {
+        // create the tweet
+        const { item } = await client.database(databaseId).container(containerId).items.create(tweet);
+        console.log(`Created item with id: ${item.id}\n`);             
+    
+    }  
+        
+}
 
 // let's read the database
+// async function with returns AsyncFunction object; promises behaviour (use .then() etc...)
 async function readDatabase() {
+    // ES6 destructuring to take out body from the JSON and call it databaseDefinition
     const { body: databaseDefinition } = await client.database(databaseId).read();
     console.log(`Reading database: ${databaseDefinition.id}\n`);
 }
 
 // let's read the container
 async function readContainer() {
+    // await to wait for the promise - need to obtain containerDefinition before proceeding
     const { body: containerDefinition } = await client.database(databaseId).container(containerId).read();
     console.log(`Reading container: ${containerDefinition.id}\n`);
 }
 
+// query tweets by Geert Baeke
 async function queryTweets() {
     console.log(`Querying container: ${containerId}`);
 
-    // query to return all tweets by Geert Baeke
     const querySpec = {
         query: "SELECT VALUE c.text FROM c WHERE c.sender = @sender",
         parameters: [
@@ -36,12 +69,25 @@ async function queryTweets() {
     };
 
     const { result: results } = await client.database(databaseId).container(containerId).items.query(querySpec).toArray();
+    // of is ES 6 syntax for iteration
     for (var queryResult of results) {
         let resultString = JSON.stringify(queryResult);
         console.log(`\t${resultString}\n`);
     }
-};
+}
 
-readDatabase()
+async function cleanup() {
+    await client.database(databaseId).delete();
+
+    
+}
+
+createDatabase()
+    .then(() => readDatabase())
+    .then(() => createContainer())
     .then(() => readContainer())
-    .then(() => queryTweets());
+    .then(() => createTweets())
+    .then(() => queryTweets())
+    .then(() => cleanup())
+    .then(() => { console.log("Great success"); process.exit(0) })
+    .catch((error) => { console.log(JSON.stringify(error));process.exit(1)});
---
+++ cosmosdb/tweets.js
@@ -0,0 +1,124 @@
+const tweets = [
+    {
+        "sender": "utkarsh chopra",
+        "text": "RT @tw_male: Kennismaking met Cowboy e-bike tijdens #xylosinspire https://t.co/pXMTnY7RBW",
+        "score": 0.5,
+        "sentiment": "neutral"
+    },
+    {
+        "sender": "Martin Lemmens",
+        "text": "Kennismaking met Cowboy e-bike tijdens #xylosinspire https://t.co/pXMTnY7RBW",
+        "score": 0.7002599835395813,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Jamie Cummins",
+        "text": "#XylosInspire I want the robot!",
+        "score": 0.17240095138549805,
+        "sentiment": "negative"
+    },
+    {
+        "sender": "ICT LECOT",
+        "text": "RT @XylosICT: Before you start #MicrosoftTeams, make sure you have a clear governance strategy with guidelines, policies and a #changemanag…",
+        "score": 0.9347630739212036,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Pekka Walkama",
+        "text": "RT @valointranet: Full house watching @knutrelbemoe’s demo of Modern Valo. Full multilingual support is a true killer feature in 🇧🇪 Belgium…",
+        "score": 0.9515654444694519,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Neolearning",
+        "text": "RT @Keuningsje: Learning is not just about knowledge. It's about 'sense making'. Thanks for the inspiring session @julianstodd 👌 #sociallea…",
+        "score": 0.9718382358551025,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Neolearning",
+        "text": "RT @XylosICT: YES: You can WIN our Mindstorm buddy today by using the hashtag #xylosinspire on social media. 🤖 #xylosinspire #techroom http…",
+        "score": 0.7608335018157959,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Michael Vonlanthen",
+        "text": "RT @valointranet: Still time left to see the world’s first #Modern #SharePoint #intranet at the Valo booth #xylosinspire https://t.co/4LMVU…",
+        "score": 0.5,
+        "sentiment": "neutral"
+    },
+    {
+        "sender": "Jan Van Belle",
+        "text": "RT @XylosICT: We overestimate #technology in the short term and underestimate technology in the long term - Arthur C. Clarke. #xylosinspire…",
+        "score": 0.5,
+        "sentiment": "neutral"
+    },
+    {
+        "sender": "Karl G. Schneider",
+        "text": "RT @knutrelbemoe: Really interesting discussions around Modern #SharePoint and #Valomodern #intranet #multilingual at #xylosinspire https:/…",
+        "score": 0.929510772228241,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Valo Intranet",
+        "text": "RT @knutrelbemoe: Really interesting discussions around Modern #SharePoint and #Valomodern #intranet #multilingual at #xylosinspire https:/…",
+        "score": 0.929510772228241,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "ADMbelgium",
+        "text": "RT @FDierckx: Meet the expert session about AI during #xylosinspire with @GeertBaeke and Pieter Sap more Great sessions this afternoon.0 ht…",
+        "score": 0.8498128056526184,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Neolearning",
+        "text": "RT @XylosICT: Please reduce context switching in the workplace = constantly being interrupted for a short period of time. #XylosInspire #ne…",
+        "score": 0.20895007252693176,
+        "sentiment": "negative"
+    },
+    {
+        "sender": "Julian Stodd",
+        "text": "RT @Neolearningexp: Learning is not just about knowledge. It's about 'sense making'. Thanks for the inspiring session @julianstodd 👌 #socia…",
+        "score": 0.9718382358551025,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Neolearning",
+        "text": "Learning is not just about knowledge. It's about 'sense making'. Thanks for the inspiring session @julianstodd 👌 #sociallearning #xylosinspire",
+        "score": 0.9686607122421265,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Katrien Hebb",
+        "text": "#apps die je #HRbeleid en #internecommunicatie versterken #XylosInspire inspireert! https://t.co/FZDyg9dh99",
+        "score": 0.09034916758537292,
+        "sentiment": "negative"
+    },
+    {
+        "sender": "bart schauwers",
+        "text": "RT @XylosICT: We overestimate #technology in the short term and underestimate technology in the long term - Arthur C. Clarke. #xylosinspire…",
+        "score": 0.5,
+        "sentiment": "neutral"
+    },
+    {
+        "sender": "utkarsh chopra",
+        "text": "Smart bike doing the rounds here #xylosinspire https://t.co/KyOlHmnEW6",
+        "score": 0.913421094417572,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Xylos",
+        "text": "Got some matches? #xylosinspire #getconnected https://t.co/Q9g1xppuQp",
+        "score": 0.8298916816711426,
+        "sentiment": "positive"
+    },
+    {
+        "sender": "Geert Baeke",
+        "text": "RT @FDierckx: Looking for a partner to implement your own company’s ‘chat bot’ ?\n#xylosict can help you. @GeertBaeke explained this in deta…",
+        "score": 0.7787208557128906,
+        "sentiment": "positive"
+    }
+]
+
+module.exports = tweets;
